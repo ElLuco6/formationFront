195000c5ff92350cbb6351e433c3620d5a052f38
Joris Pader <miisterone27@gmail.com>
2024-11-21 19:37:26 +0100

    Fixe route 3001 + add new design

--- formation/src/pages/CreateFormationPage.tsx
+++ formation/src/pages/CreateFormationPage.tsx
@@ -19,7 +19,7 @@ const CreateFormationPage: React.FC = () => {
         const formationData = { title, type, duration, description, price };
 
         try {
-            const response = await fetch('http://localhost:4200/formations', {
+            const response = await fetch('http://localhost:3001/formations', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formationData),
